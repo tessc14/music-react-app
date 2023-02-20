d5c53ec10c7691db0308ad64590348abbf4a69e0
tessc14 <tessc14@gmail.com>
2023-02-20 15:54:15 +0000

    add header component

--- music/src/App.jsx
+++ music/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import React from 'react'
+import Header from './Header.jsx'
 
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
 
   return (
     <div className="App">
-      
+        <Header />
     </div>
   )
 }
---
+++ music/src/Header.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const Header = () => {
+    
+
+    return (
+        <>
+            <h1>Artist's profile</h1>
+            <h2>Music type</h2>
+            <p>Bio</p>
+        </>
+    )
+}
+
+export default Header
