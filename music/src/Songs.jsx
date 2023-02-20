314fdccc54dcf3838351987cec54b7c35522ecd2
tessc14 <tessc14@gmail.com>
2023-02-20 17:11:48 +0000

    add like and unlike toggle

--- music/src/Songs.jsx
+++ music/src/Songs.jsx
@@ -2,7 +2,20 @@ import React, { useState } from 'react'
 
 
 const Songs = () => {
-    
+
+    const [favourite, setFavourite] = useState(false)
+
+    const addFavourite = (e) => {
+        
+        setFavourite(!favourite)
+        if (!favourite) {
+        e.target.style.backgroundColor='cadetblue'
+        e.target.textContent='Unlike'
+        } else {
+            e.target.style.backgroundColor='white'
+            e.target.textContent='Like'
+        }
+    }
 
     return (
         <>
@@ -10,45 +23,58 @@ const Songs = () => {
             <div className="songs">
                 <div className="song">
                     <h3>Song title</h3>
+                    {favourite && (<p></p>)}
                     <h4>Album</h4>
                     <p>release date</p>
-                    <button>Favourite</button>
+                    <button
+                        onClick={addFavourite}
+                        >Like
+                    </button>
                 </div>
                 <div className="song">
                     <h3>Song title</h3>
                     <h4>Album</h4>
                     <p>release date</p>
-                    <button>Favourite</button>
+                    <button
+                        onClick={ addFavourite }
+                        >Like
+                    </button>
                 </div>
                 <div className="song">
                     <h3>Song title</h3>
                     <h4>Album</h4>
                     <p>release date</p>
-                    <button>Favourite</button>
+                    <button
+                        onClick={ addFavourite }
+                        >Like
+                    </button>
                 </div>
                 <div className="song">
                     <h3>Song title</h3>
                     <h4>Album</h4>
                     <p>release date</p>
-                    <button>Favourite</button>
+                    <button
+                        onClick={ addFavourite }
+                        >Like
+                    </button>
                 </div>
                 <div className="song">
                     <h3>Song title</h3>
                     <h4>Album</h4>
                     <p>release date</p>
-                    <button>Favourite</button>
+                    <button
+                        onClick={ addFavourite }
+                        >Like
+                    </button>
                 </div>
                 <div className="song">
                     <h3>Song title</h3>
                     <h4>Album</h4>
                     <p>release date</p>
-                    <button>Favourite</button>
-                </div>
-                <div className="song">
-                    <h3>Song title</h3>
-                    <h4>Album</h4>
-                    <p>release date</p>
-                    <button>Favourite</button>
+                    <button
+                        onClick={ addFavourite }
+                        >Like
+                    </button>
                 </div>
             </div>
             
