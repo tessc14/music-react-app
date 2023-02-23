49c8483e0e2296a22c11fe0fd75adb2c0112558b
tessc14 <tessc14@gmail.com>
2023-02-23 12:24:52 +0000

    protect routes

--- music/src/App.jsx
+++ music/src/App.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import ProtectedRoute from './routes';
+import { AuthProvider } from './contexts';
 import { NavBar } from './layout'
 import * as Pages from './pages'
 import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Pages.Home />}></Route>
-        <Route path="/billie" element={<Pages.Billie />}></Route>
-        <Route path="/login" element={<Pages.Login />}/>
+    <AuthProvider>
+      <div className="App">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<ProtectedRoute redirectTo='/login' />} >
+            <Route index element={<Pages.Home />}/>
+            <Route path="/billie" element={<Pages.Billie />}/>
+          </Route>
+          
+          <Route path="/login" element={<Pages.Login />}/>
 
-      </Routes>
-    </div>
+        </Routes>
+      </div>
+    </AuthProvider>
+
   )
 }
 
